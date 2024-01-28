05de9b4689924c6a5d40be7cd12df40a161b6890
ariad.c <ariad.c@LT-ARIAD>
2024-01-28 09:31:28 +0300

    HashPassword

--- Tetco.JamaaAgent.API/API/CrossCuttings/Authorization/IAgentApiKeyValidator.cs
+++ Tetco.JamaaAgent.API/API/CrossCuttings/Authorization/IAgentApiKeyValidator.cs
@@ -1,4 +1,8 @@
-﻿namespace API.CrossCuttings.Authorization
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.CrossCuttings.Authorization
 {
     public interface IAgentApiKeyValidator
     {
@@ -13,10 +17,30 @@
             _configuration = configuration;
         }
 
-        public bool IsValid(string apiKey)
+        //public bool IsValid(string apiKey)
+        //{
+        //    //this could be check from database instead of appsettings configuration or any data source
+        //    return _configuration.GetValue<string>("Auth:AgentKey") == apiKey;
+        //}
+
+        public  bool IsValid(string apiKey)
         {
-            //this could be check from database instead of appsettings configuration or any data source
-            return _configuration.GetValue<string>("Auth:AgentKey") == apiKey;
+            var storedHashedPassword = _configuration.GetValue<string>("Auth:AgentKey");
+
+            // Hash the entered password using the same algorithm as during the initial hashing
+            string hashedEnteredPassword = HashPassword(apiKey);
+
+            // Compare the entered hashed password with the stored hashed password
+            return string.Equals(hashedEnteredPassword, storedHashedPassword, StringComparison.Ordinal);
+        }
+
+        public static string HashPassword(string apiKey)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(apiKey));
+                return Convert.ToBase64String(hashedBytes);
+            }
         }
     }
 }
